18a5ecde8302196f6dc9763cee7ad29403534332
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2024-10-11 13:31:02 -0300

    ECIR testing v3

--- src/ecir.py
+++ src/ecir.py
@@ -1,8 +1,10 @@
+import os
 import pickle
 from sys import argv
-from threading import Thread
+from threading import Event, Thread
 
-from data import Dataset
+import numpy as np
+
 from knns.bkmeans_hnsw import BisectingKmeansHNSW
 from knns.exhaustive import ExhaustiveKnn
 from knns.hnsw import HNSW
@@ -12,26 +14,44 @@ BATCH_SIZE = 360
 MAX_PCS = 3
 EXPERIMENT_SIZE = 10000
 
-def index_exhaustive(dataset, seed):
+def load_db_embeddings(dataset_name):
+    if os.path.isfile(f"../data/tfds_db_{dataset_name}.npy"):
+        db = np.load(f"../data/tfds_db_{dataset_name}.npy").tolist()
+        db = [np.array(i) for i in db]
+    else:
+        import tensorflow_datasets as tfds
+        tfds_db = list(tfds.load(dataset_name, split='database'))
+        db = [np.array(i["embedding"]) for i in tfds_db]
+        del tfds_db
+        np.save(f"../data/tfds_db_{dataset_name}.npy", db)
+    return db
+
+def index_exhaustive(seed, loading_event):
+    db_embeddings = load_db_embeddings(DB_NAME)
+    loading_event.set()
+
     print(f"indexing exhaustive with seed={seed}")
 
     exhaustive = ExhaustiveKnn()
-    exhaustive.insert(dataset.get_db_embeddings())
+    exhaustive.insert(db_embeddings)
     with open(f'../data/exhaustive_{DB_NAME}_seed{seed}.pkl', 'wb') as file:
         pickle.dump(exhaustive, file, pickle.HIGHEST_PROTOCOL)
 
-def index_hnsw_and_hcnsw(dataset, seed):
+def index_hnsw_and_hcnsw(seed, loading_event):
+    db_embeddings = load_db_embeddings(DB_NAME)
+    loading_event.set()
+
     print(f"indexing hnsw with seed={seed}")
 
-    hnsw = HNSW(m=20, ef_construction=100, ef=100, seed=seed)
-    hnsw.insert(dataset.get_db_embeddings())
+    hnsw = HNSW(m=20, ef_construction=100, ef=100, random_seed=seed)
+    hnsw.insert(db_embeddings)
     with open(f'../data/hnsw_{DB_NAME}_seed{seed}.pkl', 'wb') as file:
         pickle.dump(hnsw, file, pickle.HIGHEST_PROTOCOL)
 
     print(f"indexing hcnsw with seed={seed}")
 
     hcnsw = BisectingKmeansHNSW(m=20, ef_construction=100, ef=100, max_clusters=1024, random_seed=seed)
-    hcnsw.insert(dataset.get_db_embeddings())
+    hcnsw.insert(db_embeddings)
     with open(f'../data/hcnsw_{DB_NAME}_seed{seed}.pkl', 'wb') as file:
         pickle.dump(hcnsw, file, pickle.HIGHEST_PROTOCOL)
 
@@ -39,21 +59,19 @@ if __name__=="__main__":
     pc_id = int(argv[1])
     thread_count = int(argv[2])
     type = argv[3]
-
-    dataset = Dataset()
-    dataset.load_from_tfds(DB_NAME) 
-
     for iteration_id, batch_start in enumerate(range(0, EXPERIMENT_SIZE, BATCH_SIZE)):
         if iteration_id%MAX_PCS == pc_id:
             for first_seed in range(batch_start, batch_start+BATCH_SIZE, thread_count):
                 seeds = range(first_seed, first_seed+thread_count)               
                 threads = []
                 for seed in seeds:
+                    loading_event = Event()
                     if type == 'test':
-                        thread = Thread(target=index_exhaustive, args=(dataset, seed, ))
+                        thread = Thread(target=index_exhaustive, args=(seed, loading_event, ))
                     else:
-                        thread = Thread(target=index_hnsw_and_hcnsw, args=(dataset, seed, ))
+                        thread = Thread(target=index_hnsw_and_hcnsw, args=(seed, loading_event, ))
                     threads.append(thread)
                     thread.start()
+                    loading_event.wait()
                 for thread in threads:
                     thread.join()
